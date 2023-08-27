68e1b20159114b58f82c9dac3e591922038cc886
IlyaKovalchukKIS <mrlivedance@vk.com>
2023-08-28 02:11:43 +0300

    написан класс миксин с подключением стиля и вынесены в него запрещенные слова для создания товаров и категорий

--- catalog/forms.py
+++ catalog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from catalog.models import Product, Category
 
 
-class ProductForm(forms.ModelForm):
+class StyleMixin:
     __forbidden_words = ['казино',
                          'криптовалюта',
                          'крипта',
@@ -15,8 +15,12 @@ class ProductForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.forbidden_words = self.__forbidden_words
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(StyleMixin, forms.ModelForm):
 
     class Meta:
         model = Product
@@ -27,27 +31,13 @@ class ProductForm(forms.ModelForm):
         name = cleaned_data.get('name')
         description = cleaned_data.get('description')
 
-        for word in self.__forbidden_words:
+        for word in self.forbidden_words:
             if word in name.lower() or word in description.lower():
                 raise forms.ValidationError(f'В названии или в описании использовано запрещенное слово')
         return cleaned_data
 
 
-class CategoryForm(forms.ModelForm):
-    __forbidden_words = ['казино',
-                         'криптовалюта',
-                         'крипта',
-                         'биржа',
-                         'дешево',
-                         'бесплатно',
-                         'обман',
-                         'полиция',
-                         'радар']
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
+class CategoryForm(StyleMixin, forms.ModelForm):
 
     class Meta:
         model = Category
@@ -58,7 +48,7 @@ class CategoryForm(forms.ModelForm):
         name = cleaned_data.get('name')
         description = cleaned_data.get('description')
 
-        for word in self.__forbidden_words:
+        for word in self.forbidden_words:
             if word in name.lower() or word in description.lower():
                 raise forms.ValidationError(f'В названии или в описании использовано запрещенное слово')
         return cleaned_data
