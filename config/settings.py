fcc467d487dcabba17e6bf291b058a7a976c67af
IlyaKovalchukKIS <mrlivedance@vk.com>
2023-08-28 02:18:24 +0300

    добавлены настройки для перенаправления пользоввателя послее входа и выхода из профиля

--- config/settings.py
+++ config/settings.py
@@ -135,3 +135,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR / 'media/')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
