aeac4e2958568373338e5fa78dd79eb59307bed9
IlyaKovalchukKIS <mrlivedance@vk.com>
2023-08-27 12:44:42 +0300

    написаны url для товаров и категорий

---
+++ catalog/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+from catalog.apps import CatalogConfig
+from catalog.views import index, ProductCreateView, ProductListView, ProductDetailView, ProductUpdateView, \
+    ProductDeleteView, CategoryCreateView, CategoryDeleteView, CategoryUpdateView, CategoryDetailView, CategoryListView
+
+app_name = CatalogConfig.name
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('product/create/', ProductCreateView.as_view(), name='product_create'),
+    path('product/list/', ProductListView.as_view(), name='product_list'),
+    path('product/view/<int:pk>', ProductDetailView.as_view(), name='product_view'),
+    path('product/edit/<int:pk>', ProductUpdateView.as_view(), name='product_edit'),
+    path('product/delete/<int:pk>', ProductDeleteView.as_view(), name='product_delete'),
+
+    path('category/create/', CategoryCreateView.as_view(), name='category_create'),
+    path('category/list/',  CategoryListView.as_view(), name='category_list'),
+    path('category/view/<int:pk>',  ProductListView.as_view(), name='category_view'),
+    path('category/edit/<int:pk>',  CategoryUpdateView.as_view(), name='category_edit'),
+    path('category/delete/<int:pk>', CategoryDeleteView.as_view(), name='category_delete'),]
