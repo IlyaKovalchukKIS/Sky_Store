c6d234b7574eeff32329c3ff02ff48df9190cd27
IlyaKovalchukKIS <mrlivedance@vk.com>
2023-08-28 13:06:13 +0300

    добавлен сброс пароля по почте для восстановления доступа

--- users/views.py
+++ users/views.py
@@ -1,9 +1,15 @@
+import os
+import random
+
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.core.mail import send_mail
+
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView
 from django.contrib.auth.views import LoginView
 
+from config import settings
 from users.forms import UserRegisterForm, UserProfileForm
 from users.models import User
 
@@ -15,6 +21,18 @@ class RegisterView(CreateView):
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
 
+    def form_valid(self, form):
+        new_user = form.save()
+        send_mail(
+            subject='Поздравляю',
+            message='Welcom',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[new_user.email],
+            auth_user=settings.EMAIL_HOST_USER,
+            auth_password=settings.EMAIL_HOST_PASSWORD,
+        )
+        return super().form_valid(form)
+
 
 class ProfileView(UpdateView):
     model = User
@@ -25,3 +43,18 @@ class ProfileView(UpdateView):
     def get_object(self, queryset=None):
         return self.request.user
 
+
+def generate_new_password(request):
+    new_password = ''.join([str(random.randint(0, 9)) for _ in range(12)])
+    send_mail(
+        subject='Вы сменили пароль',
+        message=f'Ваш новый пароль: {new_password}',
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[request.user.email],
+        auth_user=settings.EMAIL_HOST_USER,
+        auth_password=settings.EMAIL_HOST_PASSWORD,
+    )
+
+    request.user.set_password(new_password)
+    request.user.save()
+    return redirect(reverse('catalog:index'))
